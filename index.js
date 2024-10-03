e4319da85f5b149d587d3402b6bc2ba9cd62b3b0
Calvin Owusu Bekoe <calvinbekoe7@gmail.com>
2024-10-03 15:21:52 +0000

    installed cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { bookingRouter } from './routes/all-routes.js';
 import mongoose from 'mongoose';
 import 'dotenv/config'
+import cors from 'cors'
 
 
 const bookingApp = express();
 const PORT = 3000
-await mongoose.connect(process.env.MONGO_URI)
 
+
+bookingApp.use(cors)
 bookingApp.use (express.json())
 bookingApp.use (bookingRouter)
  
@@ -18,7 +20,7 @@ bookingApp.get('/booking', (req, res, next) => {
     res.json('You booked a seat');
 });
 
-
+await mongoose.connect(process.env.MONGO_URI)
 bookingApp.listen(PORT, () => 
     console.log(`Booking is listening on port ${PORT}`)
 );
